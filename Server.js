882c7dc2e30a93d9a84029f95fad2ba5811a6b05
ErnestX <xiangspecial2012@gmail.com>
2022-10-09 16:13:11 -0400

    refactor

--- Server.js
+++ Server.js
@@ -1,25 +1,39 @@
+function initializeClient() {
+	const qrcode = require('qrcode-terminal');
+	const { Client } = require('whatsapp-web.js');
+	const client = new Client();
 
-const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+	client.on('qr', qr => {
+		qrcode.generate(qr, { small: true });
+	});
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+	client.on('ready', () => {
+		console.log('Client is ready!');
+	});
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+	client.initialize();
+}
 
-client.initialize();
+function loadDebugEvents() {
 
-client.on('message', message => {
-	console.log(message.body);
-});
+	// test receieving message
+	client.on('message', message => {
+		console.log(message.body);
+	});
+	
+	// test public reply
+	client.on('message', message => {
+		if(message.body === '!ping') {
+			message.reply('pong');
+		}
+	});
+}
 
-client.on('message', message => {
-	if(message.body === '!ping') {
-		message.reply('pong');
-	}
-});
- 
+
+//////////////// Server Starts /////////////////
+
+initializeClient();
+
+loadDebugEvents(); 
+
+
