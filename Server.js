f40ccc7182ee5a4733de9c3b787971c43dd78d83
ErnestX <xiangspecial2012@gmail.com>
2022-10-09 21:28:29 -0400

    print chat number and names

--- Server.js
+++ Server.js
@@ -7,25 +7,42 @@ loadDebugEvents();
 
 
 
-
 //////////////// Function Definitions ////////////////
 
 function createClient() {
 	const qrcode = require('qrcode-terminal');
 	const { Client } = require('whatsapp-web.js');
 	var c = new Client();
+	// const { Client, LocalAuth } = require('whatsapp-web.js');
+	// const c = new Client({
+ 	//    authStrategy: new LocalAuth()
+	// });
 
 	c.on('qr', qr => {
 		qrcode.generate(qr, { small: true });
 	});
 
 	c.on('ready', () => {
-		console.log('Client is ready!');
+		onClientReady();
 	});
 
 	c.initialize();
 
 	return c;
+}
+
+function onClientReady() {
+	console.log('Client is ready!');
+	testChats();
+}
+
+async function testChats() {
+	const allChats = await client.getChats();
+	console.log(typeof allChats);
+	console.log(allChats.length);
+	console.log(typeof allChats[0]);
+	console.log(allChats[0].id);
+	console.log(allChats[0].name);
 }
 
 function loadDebugEvents() {
@@ -48,5 +65,6 @@ function loadDebugEvents() {
 		}
 	});
 }
+
 
  
